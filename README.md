92c5d816967627680044f12693e6f9d64b7dcc81
bemadunezim <bemadunezim@miners.utep.edu>
2016-12-07 18:15:33 -0700

    Final SUB

--- README.md
+++ README.md
@@ -1,38 +1,22 @@
-# Project 3: LCD Game
-## Introduction
-Continuing your effort to help ACME Solutions provide Christmas toys, your task is now to develop a game for the LCD screen.  You should utilize the knowledge you have gained working with the buttons and buzzer.  In addition, a series of libraries have been found to assist you in creating your game.
+Project3
+Brian Madunezim
 
-## Requirements
-Your game should meet the following minimum criteria:
-
-- dynamically draw and translate shapes across the screen
-- handle colissions properly with other shapes and area boundaries
-- produce sound in relation to the state of the game
-- communicate with the player using text
-- implement a state transition in assembly
-
-Don't forget to properly document your source code and how to play the game.
-
-## Grading Criteria
-Your lab will be graded based on the level of understanding shown and demonstrated in your game (i.e. the complexity of your game).  The grading will follow a traditional letter grade scale (A, B, C, F).
-
-Consider the requirements of producing a simple game of pong.  You have the following elements:
-
-- an arena to play in
-- a ball
-- two paddles to smack the ball around
-- a score 
-
-And the game usually behaves with the following functionality:
-
-- the ball moves at a velocity and collides with the arena and paddles
-- a sound plays when a collision occurs
-- a player scores when the ball makes it to the opposite side of the screen
-- the score is updated and the game begins again
-
-Developing a pong game with the minimum features (discussed above) will earn you a grade of a C.  
-
-To earn a better grade (for pong), consider the following ideas:
-
-- shapes, colors, speeds and sizes of graphical elements change size in response to player skill
-- rendering the game in such a way that moving objects pass through (but don't erase) the score
+At the top of the file we would include the relevant headers such as the shape definitions and library for printf..
+Next we would define our constants such as the radius for the circles and the size of the paddles.
+We would then declare the global layer structures, including: ping pong table, the two paddles, and the ping pong ball.
+We would initialize the structures with the constants such as the radius and rectangle size previously declared. 
+The ping pong ball would have an initial velocity in the y direction.
+Next we would declare two score variables as u_ints one for player one and the other for player two.
+The next thing we would need to do is create a motion function which changes the position of the layer based on the 
+velocity of the layer(layers include ping pong table, the two paddles ,and the ball).
+When the ball hits a wall(detected by the x coordinate being less than 0 or greater than the width of the screen)
+the x component of the velocity is reverese. If the ball passes the top of the screen(detected by the y coordinate
+being less than zero) player 1 recieves a point, then the ball will be re-initiliazed back to the center of the screen
+if the ball passes the bottom of the screen(detected by the y coordinates being greater than the screen height)
+then player two will receive a point. If either player scores is greater than or equal to 10 that player wins and the
+the game is over. The game will then display the score for player1 at the bottom of the screen and player 2 at the top
+of the screen. The game over message will be displayed next to the winner's score. If a ball collides with a paddle
+as dected using the rectangle's radius method the y velocity component is reversed and the paddles x's velocity 
+component is added to the ball's x velocity.
+The main function contains device initilaization and the main game loop. The motion function is tied to the timer, the
+paddle x velocity component is tied to the game controls.
--- shape-motion-demo/shapemotion.c
+++ shape-motion-demo/shapemotion.c
@@ -1,5 +1,5 @@
-/** \file shapemotion.c
- *  \brief This is a simple shape motion demo.
+/** file shapemotion.c
+ *  brief This is a simple shape motion demo.
  *  This demo creates two layers containing shapes.
  *  One layer contains a rectangle and the other a circle.
  *  While the CPU is running the green LED is on, and
@@ -13,56 +13,54 @@
 #include <p2switches.h>
 #include <shape.h>
 #include <abCircle.h>
+#include <stdio.h>
+
+#define R_LENGTH 2
+#define R_WIDTH 10
+#define C1RAD 20
+#define C2RAD 12
 
 #define GREEN_LED BIT6
-
+void mlCheckCollision();
 
 AbRect rect10 = {abRectGetBounds, abRectCheck, {10,2}}; /**< 10x10 rectangle */
 AbRArrow rightArrow = {abRArrowGetBounds, abRArrowCheck, 30};
 
-AbRectOutline fieldOutline = {	/* playing field */
+AbRectOutline fieldOutline = { /* playing field */
   abRectOutlineGetBounds, abRectOutlineCheck,   
   {screenWidth/2 - 10, screenHeight/2 - 10}
 };
-
-Layer layer4 = {
-  (AbShape *)&rightArrow,
-  {(screenWidth/2)+10, (screenHeight/2)+5}, /**< bit below & right of center */
-  {0,0}, {0,0},				    /* last & next pos */
-  COLOR_PINK,
-  0
-};
   
 
-Layer layer3 = {		/**< Layer with an orange circle */
+Layer layer3 = { /**< Layer with an orange circle */
   (AbShape *)&circle8,
   {(screenWidth/2)+10, (screenHeight/2)+5}, /**< bit below & right of center */
-  {0,0}, {0,0},				    /* last & next pos */
+  {0,0}, {0,0},     /* last & next pos */
   COLOR_VIOLET,
-  &layer4,
+  0,
 };
 
 
-Layer fieldLayer = {		/* playing field as a layer */
+Layer fieldLayer = { /* playing field as a layer */
   (AbShape *) &fieldOutline,
   {screenWidth/2, screenHeight/2},/**< center */
-  {0,0}, {0,0},				    /* last & next pos */
+  {0,0}, {0,0},     /* last & next pos */
   COLOR_BLACK,
   &layer3
 };
 
-Layer layer1 = {		/**< Layer with a red square */
+Layer layer1 = { /**< Layer with a red square */
   (AbShape *)&rect10,
-  {screenWidth/2, screenHeight/2}, /**< center */
-  {0,0}, {0,0},				    /* last & next pos */
+  {screenWidth/2, screenHeight-15}, /**< center, bottom */
+  {0,0}, {0,0},     /* last & next pos */
   COLOR_RED,
   &fieldLayer,
 };
 
-Layer layer0 = {		/**< Layer with an orange circle */
+Layer layer0 = { /**< Layer with an orange circle */
   (AbShape *)&circle14,
   {(screenWidth/2)+10, (screenHeight/2)+5}, /**< bit below & right of center */
-  {0,0}, {0,0},				    /* last & next pos */
+  {0,0}, {0,0},     /* last & next pos */
   COLOR_ORANGE,
   &layer1,
 };
@@ -79,7 +77,7 @@ typedef struct MovLayer_s {
 
 /* initial value of {0,0} will be overwritten */
 MovLayer ml3 = { &layer3, {1,1}, 0 }; /**< not all layers move */
-MovLayer ml1 = { &layer1, {1,2}, &ml3 }; 
+MovLayer ml1 = { &layer1, {0,0}, &ml3 }; 
 MovLayer ml0 = { &layer0, {2,1}, &ml1 }; 
 
 
@@ -93,71 +91,120 @@ movLayerDraw(MovLayer *movLayers, Layer *layers)
   int row, col;
   MovLayer *movLayer;
 
-  and_sr(~8);			/**< disable interrupts (GIE off) */
+  and_sr(~8); /**< disable interrupts (GIE off) */
   for (movLayer = movLayers; movLayer; movLayer = movLayer->next) { /* for each moving layer */
     Layer *l = movLayer->layer;
     l->posLast = l->pos;
     l->pos = l->posNext;
   }
-  or_sr(8);			/**< disable interrupts (GIE on) */
+  or_sr(8); /**< disable interrupts (GIE on) */
 
 
   for (movLayer = movLayers; movLayer; movLayer = movLayer->next) { /* for each moving layer */
     Region bounds;
     layerGetBounds(movLayer->layer, &bounds);
     lcd_setArea(bounds.topLeft.axes[0], bounds.topLeft.axes[1], 
-		bounds.botRight.axes[0], bounds.botRight.axes[1]);
+ bounds.botRight.axes[0], bounds.botRight.axes[1]);
     for (row = bounds.topLeft.axes[1]; row <= bounds.botRight.axes[1]; row++) {
       for (col = bounds.topLeft.axes[0]; col <= bounds.botRight.axes[0]; col++) {
-	Vec2 pixelPos = {col, row};
-	u_int color = bgColor;
-	Layer *probeLayer;
-	for (probeLayer = layers; probeLayer; 
-	     probeLayer = probeLayer->next) { /* probe all layers, in order */
-	  if (abShapeCheck(probeLayer->abShape, &probeLayer->pos, &pixelPos)) {
-	    color = probeLayer->color;
-	    break; 
-	  } /* if probe check */
-	} // for checking all layers at col, row
-	lcd_writeColor(color); 
+ Vec2 pixelPos = {col, row};
+ u_int color = bgColor;
+ Layer *probeLayer;
+ for (probeLayer = layers; probeLayer; 
+      probeLayer = probeLayer->next) { /* probe all layers, in order */
+   if (abShapeCheck(probeLayer->abShape, &probeLayer->pos, &pixelPos)) {
+     color = probeLayer->color;
+     break; 
+   } /* if probe check */
+} // for checking all layers at col, row
+ lcd_writeColor(color);
       } // for col
     } // for row
+    // mlCheckCollision();//check for the collision-user created function
   } // for moving layer being updated
-}	  
+}   
 
+/*void mlCheckCollision(){
+  Region bCircle1, bCircle2, bRect;
+  layerGetBounds(&layer0, &bCircle1);
+  layerGetBounds(&layer1, &bRect);
+  layerGetBounds(&layer3, &bCircle2);
 
+  if (bCircle1.botRight.axes[0]>= bRect.topLeft.axes[0] &&
+      bCircle1.botRight.axes[0]<= bRect.botRight.axes[0]) {
+           drawString5x7(20,20, "circle1 row", COLOR_GREEN, COLOR_RED);
+  }//end 1st if
+  
+  if (bCircle1.topLeft.axes[1]>=bRect.topLeft.axes[1] &&
+      bCircle1.topLeft.axes[1]<=bRect.botRight.axes[1]){
+         drawString5x7(20,20, "circle1 left column", COLOR_GREEN, COLOR_RED);
+  }//end 2nd if
+  if (bCircle1.botRight.axes[1]>=bRect.topLeft.axes[1] && bCircle1.botRight.axes[1]<= bRect.botRight.axes[1]){
+    drawString5x7(20,20, "circle1 right column", COLOR_GREEN, COLOR_RED);
+  }//end 3rd if 
+}
+*/
 
 //Region fence = {{10,30}, {SHORT_EDGE_PIXELS-10, LONG_EDGE_PIXELS-10}}; /**< Create a fence region */
 
 /** Advances a moving shape within a fence
  *  
- *  \param ml The moving shape to be advanced
- *  \param fence The region which will serve as a boundary for ml
+ *  am ml The moving shape to be advanced
+ *  am fence The region which will serve as a boundary for ml
  */
-void mlAdvance(MovLayer *ml, Region *fence)
+
+void mlAdvance(MovLayer *ml, Region *fence, MovLayer *rect, MovLayer *circle1, MovLayer *circle2)
 {
   Vec2 newPos;
   u_char axis;
   Region shapeBoundary;
+  Region rectBoundary;
+  Region circleBoundary;
+  Vec2 circle1Pos, rectPos;
+  u_int circle1Rad;
+  int rectRad = (R_WIDTH/2);
   for (; ml; ml = ml->next) {
     vec2Add(&newPos, &ml->layer->posNext, &ml->velocity);
     abShapeGetBounds(ml->layer->abShape, &newPos, &shapeBoundary);
     for (axis = 0; axis < 2; axis ++) {
       if ((shapeBoundary.topLeft.axes[axis] < fence->topLeft.axes[axis]) ||
-	  (shapeBoundary.botRight.axes[axis] > fence->botRight.axes[axis]) ) {
-	int velocity = ml->velocity.axes[axis] = -ml->velocity.axes[axis];
-	newPos.axes[axis] += (2*velocity);
-      }	/**< if outside of fence */
+   (shapeBoundary.botRight.axes[axis] > fence->botRight.axes[axis]) ) {
+ int velocity = ml->velocity.axes[axis] = -ml->velocity.axes[axis];
+ newPos.axes[axis] += (2*velocity);
+      } /**< if outside of fence */
     } /**< for axis */
     ml->layer->posNext = newPos;
   } /**< for ml */
+
+  // drawString5x7(circle1->layer->pos.axes[0], circle1->layer->pos.axes[1], "center should be here", COLOR_GREEN, COLOR_RED);
+  if( (circle1->layer->pos.axes[1] + C1RAD >= rect->layer->pos.axes[1] + R_LENGTH) &&
+      (circle1->layer->pos.axes[1] - C1RAD <= rect->layer->pos.axes[1]- R_LENGTH) &&
+      (circle1->layer->pos.axes[0] - rectRad - C1RAD <= rect->layer->pos.axes[0] + R_WIDTH)
+      
+      ){
+    // if(circle1Pos.axes[0] + circle1Rad > rectBoundary.topLeft.axes[0]){
+              drawString5x7(10,10, "YOU LOSE! ;P", COLOR_GREEN, COLOR_RED);
+	      
+    //   }//end circle1 statement 
+}
+  if( (circle2->layer->pos.axes[1] + C2RAD >= rect->layer->pos.axes[1] + R_LENGTH) &&
+      (circle2->layer->pos.axes[1] - C2RAD <= rect->layer->pos.axes[1]- R_LENGTH) &&
+      (circle2->layer->pos.axes[0] - rectRad - C2RAD <= rect->layer->pos.axes[0] + R_WIDTH)
+      
+      ){
+    // if(circle1Pos.axes[0] + circle1Rad > rectBoundary.topLeft.axes[0]){
+              drawString5x7(10,10, "YOU LOSE! ;P", COLOR_GREEN, COLOR_RED);
+	      
+    //   }//end circle1 statement 
+}
+
 }
 
 
 u_int bgColor = COLOR_BLUE;     /**< The background color */
 int redrawScreen = 1;           /**< Boolean for whether screen needs to be redrawn */
 
-Region fieldFence;		/**< fence around playing field  */
+Region fieldFence; /**< fence around playing field  */
 
 
 /** Initializes everything, enables interrupts and green LED, 
@@ -165,34 +212,41 @@ Region fieldFence;		/**< fence around playing field  */
  */
 void main()
 {
-  P1DIR |= GREEN_LED;		/**< Green led on when CPU on */		
+  P1DIR |= GREEN_LED; /**< Green led on when CPU on */
   P1OUT |= GREEN_LED;
 
   configureClocks();
   lcd_init();
   shapeInit();
-  p2sw_init(1);
+  p2sw_init(15);
 
   shapeInit();
 
   layerInit(&layer0);
   layerDraw(&layer0);
 
-
   layerGetBounds(&fieldLayer, &fieldFence);
 
 
   enableWDTInterrupts();      /**< enable periodic interrupt */
-  or_sr(0x8);	              /**< GIE (enable interrupts) */
+  or_sr(0x8);               /**< GIE (enable interrupts) */
 
 
-  for(;;) { 
+  for(;;) { //game loop
     while (!redrawScreen) { /**< Pause CPU if screen doesn't need updating */
       P1OUT &= ~GREEN_LED;    /**< Green led off witHo CPU */
-      or_sr(0x10);	      /**< CPU OFF */
+      or_sr(0x10);       /**< CPU OFF */
     }
     P1OUT |= GREEN_LED;       /**< Green led on when CPU on */
     redrawScreen = 0;
+
+    u_int switches = p2sw_read();
+     if(switches & BIT0){
+               ml1.velocity.axes[0] = 1;      
+     }
+     if(switches & BIT1){
+        ml1.velocity.axes[0] = -1;  
+     }
     movLayerDraw(&ml0, &layer0);
   }
 }
@@ -201,13 +255,15 @@ void main()
 void wdt_c_handler()
 {
   static short count = 0;
-  P1OUT |= GREEN_LED;		      /**< Green LED on when cpu on */
+  P1OUT |= GREEN_LED;       /**< Green LED on when cpu on */
   count ++;
   if (count == 15) {
-    mlAdvance(&ml0, &fieldFence);
+    mlAdvance(&ml0, &fieldFence,&ml1,&ml0, &ml3);
     if (p2sw_read())
       redrawScreen = 1;
     count = 0;
   } 
-  P1OUT &= ~GREEN_LED;		    /**< Green LED off when cpu off */
+  P1OUT &= ~GREEN_LED;     /**< Green LED off when cpu off */
 }
+
+
